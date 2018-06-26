326a8bcfc9850c2ea3fd838a1b69f538b46a396c
mingcheng <zhumc11@gmail.com>
2018-06-26 16:24:34 -0700

    day 1 effort

--- scripts/pyoperantctl.py
+++ scripts/pyoperantctl.py
@@ -33,4 +33,37 @@ def readfile(filename_in, numBoxes_in){
 
     if re.match('/^\#/', line):
       #print("ignoring a comment line\n")
-    elif re.match('~ /\S+/')
+    elif re.match('~ /\S+/'):
+
+        fields = []
+        for word in line.split('/\s+/')
+            fields.append(word)
+        if len(fields)==5:
+            if(fields[0]>0 && fields[0]<=numBoxes):
+                if(boxdone[fields[0] - 1] == 0):
+                    boxdone[fields[0] - 1] =1
+                
+                    f = 0
+                    for string in fields:
+                        current = string.strip()
+                        while re.match('~ /<(\d+)>/', current) && match_0> 0 && match_1 <= f:
+                            # print "\n"$current matched for $1\n"
+                            # current = ~ s/<(\d+)>/$fixed[$realLineNumNum][match_1-1]
+                        fixed[realLineNum[f] = current
+                        # print ("fixed[realLineNum[f] \t")
+                        f++
+                print ("\n")
+                realLineNum++
+
+                else:
+                    print("ignoring an additional line for box %d in the file", fields[0])
+            else:
+                print("ignoring a line for box %d in the file", fields[0])
+
+        else:
+            print("ignoring a line in the file %s that contains something other than 5 columns")
+    else:
+        print("ignoring a blank line\n")
+    j++
+return fixed
+    
